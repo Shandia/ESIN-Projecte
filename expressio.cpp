a0c1db17bbcd408d1aa29aafc7a5bcd750c03d6e
jooanna4 <joana.bruguera@gmail.com>
2022-12-19 22:14:55 +0100

    .

--- expressio.cpp
+++ expressio.cpp
@@ -1,6 +1,14 @@
 #include "expressio.hpp"
 
-expressio(const token t = token()) throw(error) {
+// Metodes privats
+node* expressio::insert(node *n, token t) {
+    if (n == nullptr) 
+        return new node(t);
+    
+}
+
+// Metodes publics
+expressio::expressio(const token t = token()) throw(error) {
     if (t.c_actual >= NULLTOK and t.c_actual <= VAR_PERCENTATGE) {
         _arrel = new node;
         _arrel->info = t;
@@ -10,19 +18,23 @@ expressio(const token t = token()) throw(error) {
     else throw(ErrorSintactic);
 }
 
-expressio(const list<token> & l) throw(error) {
-
+expressio::expressio(const list<token> & l) throw(error) {
+    if (l.empty()) throw (ErrorSintactic);
+    list<token>::iterator it;
+    for (it = l.begin(); it != l.end(); it++) {
+        _arrel = insert ( _arrel , *it);
+    }
 }
 
-expressio(const expressio & e) throw(error) {
+expressio::expressio(const expressio & e) throw(error) {
     
 }
 
-expressio & operator=(const expressio & e) throw(error) {
+expressio::expressio & operator=(const expressio & e) throw(error) {
 
 }
 
-~expressio() throw(error) {
+expressio::~expressio() throw(error) {
 
 }
 
@@ -30,30 +42,30 @@ operator bool() const throw() {
 
 }
 
-bool operator==(const expressio & e) const throw() {
+bool expressio::operator==(const expressio & e) const throw() {
 
 }
 
-bool operator!=(const expressio & e) const throw() {
+bool expressio::operator!=(const expressio & e) const throw() {
 
 }
 
-void vars(list<string> & l) const throw(error) {
+void expressio::vars(list<string> & l) const throw(error) {
 
 }
 
-void apply_substitution(const string & v, const expressio & e) throw(error) {
+void expressio::apply_substitution(const string & v, const expressio & e) throw(error) {
 
 }
 
-void simplify_one_step() throw(error) {
+void expressio::simplify_one_step() throw(error) {
 
 }
 
-void simplify() throw(error) {
+void expressio::simplify() throw(error) {
 
 }
 
-void list_of_tokens(list<token> & lt) throw(error) {
+void expressio::list_of_tokens(list<token> & lt) throw(error) {
     
 }
