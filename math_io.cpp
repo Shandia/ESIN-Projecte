36a0064560d0646b37583075f99f2cbde7298aba
jooanna4 <joana.bruguera@gmail.com>
2023-01-08 21:58:53 +0100

    .

--- math_io.cpp
+++ math_io.cpp
@@ -1,1 +1,18 @@
 #include "math_io.hpp"
+
+void math_io::scan(const string & s, list<token> & lt) throw(error) {
+    int size = s.size();
+    
+    for(int i = 0; i < size; i++) {
+        if(s[i] == '+') {
+            if (s[i+1] == ) lt.push_back(token(SUMA));
+        }
+        else if (s[i] == '-') {
+            lt.push_back(token(RESTA));
+        }
+        else if (s[i] == '*')
+            lt.push_back(token(MULTIPLICACIO));
+        else if (s[i] == '/')
+            lt.push_back(token(DIVISIO));
+    }
+}
